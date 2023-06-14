7cc58fd81f66aac68a01af7a467efcc8e89b4b20
nethelyn <135667235+nethelyn@users.noreply.github.com>
2023-06-14 19:49:12 -0300

    Add files via upload

---
+++ aula_bd_44.sql
@@ -0,0 +1,24 @@
+select * from acoes where papel= "bbas3" or papel= "petr3";
+
+select * from acoes where not dividend_yield < 0.12;
+
+select papel, cotacao from acoes where cotacao between 30 and 50;
+
+select papel, cotacao from acoes where cotacao between 30 and 50 order by cotacao;
+
+select * from acoes where dividend_yield >0.12 or (pvp <=5 and pvp>0);
+
+select * from acoes where not dividend_yield <0.12 order by dividend_yield desc;
+
+select * from acoes where not cotacao < 30;
+
+select * from acoes where not patrimonio_liquido < 100000000000;
+
+select * from acoes where not dividend_yield >0.12 and not cotacao < 30 and not patrimonio_liquido < 1000000000 order by dividend_yield desc;
+
+select * from acoes where not dividend_yield <0.12 and (patrimonio_liquido> 1000000000) and (cotacao between 30 and 50) order by cotacao desc limit 5;
+
+select papel, cotacao, patrimonio_liquido from acoes where not dividend_yield <0.12 and patrimonio_liquido < 1000000000 and cotacao between 30 and 50 order by cotacao desc limit 5;
+
+
+
